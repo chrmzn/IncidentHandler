9c1e7ff6c2f591e133b9a7440bc8e052d9abc861
Chris OBrien <chris@theobriens.co.nz>
2013-02-02 08:37:32 +0000

    Fleshing out the call and sms functionality

--- IncidentHandler/ih/Handlers/IncidentHandler.py
+++ IncidentHandler/ih/Handlers/IncidentHandler.py
@@ -1,8 +1,10 @@
 import json
 import logging
+import datetime
 
 from twisted.web.resource import Resource
 from apscheduler.scheduler import Scheduler
+from ih.Parsers.ResponseParser import ResponseParser as rp
 
 class IncidentHandler(Resource):
 
@@ -12,6 +14,7 @@ class IncidentHandler(Resource):
         self._tc = tc
         self._config = config
         self._Sched = Scheduler()
+        self._Sched.start()
 
         self._notifcationState = {}
 
@@ -21,16 +24,55 @@ class IncidentHandler(Resource):
         logging.info("Beginning notification process")
         if self._incidentDetails["SMS"].lower() == "true":
             self._notificationState["SMS"] = {}
-            logging.info("Incident is set to send SMS messages, generating an ID for text message response")
-            self._smsPin = self.generate_pins(4, 1) 
-            smsMessage = self._incidentDetails["Message"] + "\nPlease respond with the PIN: %s to accept this ticket" % str(self._smsPin))
-            logging.info("Pin is set to %s" % str(self._smsPin))
-            for user in self._config.Users():
-                if user["SMS"] == "True":
-                    res = self._tc.sendMessage(self._config.TwilioNumber(), user["Phone"], smsMessage)
-                    
-                   
-            
+            self._sendSMS()
+        if self._incidentDetails["Call"].lower() == "true" and self._incidentDetails["SMS"].lower() == "true" :
+            self._notificationState["Call"] = {}
+            logging.info("Beginning calls in 5 minutes time")
+            self._callJob = self._Sched.add_date_job(self._sendCalls, (datetime.datetime.now() - datetime.timedelta(minutes=5)))
+        elif self._incidentDetails["Call"].lower() == "true":
+            self._sendCalls()
+    
+    def _sendSMS(self):
+        logging.info("Incident is set to send SMS messages, generating an ID for text message response")
+        self._smsPin = self.generate_pins(4, 1)
+        smsMessage = self._incidentDetails["Message"] + "\nPlease respond with the PIN: %s to accept this ticket" % str(self._smsPin))
+        logging.info("SMS Pin is set to %s" % str(self._smsPin))
+        self._incidentDetails["SMS_Pin"] = self._smsPin
+        for user in self._config.Users():
+            if user["SMS"] == "True":
+                logging.info("User '%s' is set to recieve SMS messages...Sending..." % user["Name"])
+                res = self._tc.sendMessage(from_=self._config.TwilioNumber(), 
+                                           to=user["Phone"], 
+                                           body=smsMessage)
+            else:
+                logging.info("User '%s' is not set to recieve SMS messages...Skipping..." % user["Name"])
+
+    def _sendCalls(self):
+        logging.info("Incident is set to send SMS messages, generating an ID for text message response")
+        self._callPin = self.generate_pins(4, 1)
+        logging.info("Call Pin is set to %s" % str(self._smsPin))
+        responseArray = [{ "Say" : "New Incident"},
+                         { "Say" : self._incidentDetails["Message"]},
+                         { "Gather" : { "say"         : "Please key the following digits to accept the task " + str(self._callPin),
+                                        "action"      : "http://host:port/IncidentHandler/Id/PinCheck",
+                                        "method"      : "GET",
+                                        "finishOnKey" : "*"},
+                         { "Say" : "You entered no digits. Goodbye!"}]
+        responseArray = rp(responseArray)
+        for user in self._config.Users():
+            if user["Call"] == "True":
+                logging.info("User '%s' is set to recieve calls...Sending..." % user["Name"])
+                res = self._tc.sendCall(from_=self._config.TwilioNumber(), 
+                                        to=user["Phone"], 
+                                        url="http://host:port/IncidentHandler/Id/CallResponse")
+            else:
+                logging.info("User '%s' is not set to recieve calls...Skipping..." % user["Name"])
+        
+    def getSMSPin(self):
+        return self._smsPin
+        
+    def getCallPin(self):
+        return self._callPin
 
     def render_GET(self, request):
         logging.info("Call responder: " + ", ".join(request.postpath))
